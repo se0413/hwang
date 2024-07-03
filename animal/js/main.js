f3ae71d621d430d4b2f09a2ad4c39a8588bd640b
se0413 <tldms7133@naver.com>
2024-07-03 13:15:51 +0900

    0703

--- animal/js/main.js
+++ animal/js/main.js
@@ -115,6 +115,10 @@ $(document).ready(function(){
         slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
+            360: {    /* 360px 이상일때 적용 */
+            slidesPerView: 2,
+            spaceBetween: 16,
+            },
             640: {    /* 640px 이상일때 적용 */
                 slidesPerView: 2,
                 spaceBetween: 16,
@@ -139,6 +143,10 @@ $(document).ready(function(){
         slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
+            360: {    /* 360px 이상일때 적용 */
+            slidesPerView: 2,
+            spaceBetween: 16,
+            },
             640: {    /* 640px 이상일때 적용 */
                 slidesPerView: 2,
                 spaceBetween: 16,
@@ -170,5 +178,30 @@ $(document).ready(function(){
     $(this).addClass('on')
    })
 
+
+
+
+   /*fmailly swiper*/
+
+   const familly_swiper = new Swiper('.familly .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            1300: {    /* 768px 이상일때 적용 */
+                slidesPerView: 'auto',
+                spaceBetween: 24,
+            },
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView:'auto',
+                spaceBetween: 20,
+            },
+        },
+        centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.familly .btn_wrap .next',
+            prevEl: '.familly .btn_wrap .prev',
+        },
+    });
 })
 
