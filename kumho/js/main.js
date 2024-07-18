4206c294ff9c9bc88d2b676d8496b534f85e06f9
se0413 <tldms7133@naver.com>
2024-07-18 13:17:32 +0900

    0718.

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -47,4 +47,28 @@ $(document).ready(function(){
         $('.biz .list ul li').removeClass('on')
         $('.biz .list ul li').removeClass('off')
     })
+
+    const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 2,
+                spaceBetween: 24,
+            },
+            1024: {    /* 768px 이상일때 적용 */
+                slidesPerView: 3,
+                spaceBetween: 24,
+            },
+        },
+        navigation: {
+            nextEl: '.news .btn_next',
+            prevEl: '.news .btn_prev',
+        },
+        scrollbar: {
+            el: ".news .swiper-scrollbar",
+            hide: false,
+            draggable: true,
+        },
+    });
 })
