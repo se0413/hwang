2c393b0f44a6b613ea4d16f85e11e56b75f0485a
se0413 <tldms7133@naver.com>
2024-08-26 13:17:10 +0900

    0826

--- forest/js/main.js
+++ forest/js/main.js
@@ -27,6 +27,10 @@ $(document).ready(function(){
 			console.log(destination.index)
 			$('.fp_nav ul li').removeClass('active')
 			$('.fp_nav ul li').eq(destination.index).addClass('active')
+
+			if(destination.index == 1){
+				$('.tree .count span').counterUp(); /* tree페이지로 왔을때만 실행 */
+			}
 		},
 
 		responsiveWidth: 768 /* fullpage를 적용시키지 않을 모바일 사이즈 */
@@ -49,5 +53,30 @@ $(document).ready(function(){
 			},
 		},
 
+	});
+
+	const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			768: {    /* 1280px 이상일때 적용 */
+				slidesPerView: 4,
+				spaceBetween: 14,
+			},
+			1240: {    
+				slidesPerView: 3,
+				spaceBetween: 24,
+			},
+			1600: {    
+				slidesPerView: 4,
+				spaceBetween: 24,
+			},
+		},
+		centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+		// autoplay: {  /* 팝업 자동 실행 */
+		// 	delay: 2500,
+		// 	disableOnInteraction: true,
+		// }
 	});
 })
