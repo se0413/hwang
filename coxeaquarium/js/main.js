c97a79be91dafbc85754415dec976fabd7caa05c
se0413 <tldms7133@naver.com>
2024-08-29 17:54:27 +0900

    0829.2

--- coxeaquarium/js/main.js
+++ coxeaquarium/js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 			
 		},
 
-		responsiveWidth: 768
+		responsiveWidth: 1024
 	});
 
 	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
@@ -47,28 +47,6 @@ $(document).ready(function(){
         visual_swiper.autoplay.start();  /* 재생 기능 */
     })
 
-	const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
-		slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-		breakpoints: {
-			768: {    /* 1280px 이상일때 적용 */
-				slidesPerView: 3,
-				spaceBetween: 14,
-			},
-			1240: {    
-				slidesPerView: 3,
-				spaceBetween: 24,
-			},
-			1600: {    
-				slidesPerView: 4,
-				spaceBetween: 24,
-			},
-		},
-		centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-		// autoplay: {  /* 팝업 자동 실행 */
-		// 	delay: 2500,
-		// 	disableOnInteraction: true,
-		// }
-	});
+
+
 })
