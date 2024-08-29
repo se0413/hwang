7ebfb5822d71b1b515ecc15765a2bf4939c13501
se0413 <tldms7133@naver.com>
2024-08-29 13:18:17 +0900

    0829.1

--- coxeaquarium/js/main.js
+++ coxeaquarium/js/main.js
@@ -2,33 +2,22 @@ $(document).ready(function(){
 
 	const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
-		navigation: false, /* 오른쪽에 각 페이지의 paging */
+		navigation: true, /* 오른쪽에 각 페이지의 paging */
 		navigationPosition: 'left', /* 위치 */
 		navigationTooltips: ['COEX', 'INFORMATION', 'Show & Event', 'Stamp Zone', 'Photo review', 'Theme Zone', 'Location'], /* 툴팁 */
 		showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
-
-		lockAnchors: false,
-		anchors: ['visual', 'info', 'show_event', 'stamp', 'photo_review', 'theme', 'location'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
 
 		autoScrolling:true, /* 한페이지씩 스크롤 */
 		scrollHorizontally: true,
 
 		verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
-		
 		scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
 		afterLoad: function(origin, destination, direction, trigger){
-			if((destination.index == 1)||(destination.index == 3)||(destination.index == 5)){ 
-				$('.fp_nav').addClass('dark')
-			}else{
-				$('.fp_nav').removeClass('dark')
-      		}
-			console.log(destination.index)
-			$('.fp_nav ul li').removeClass('active')
-			$('.fp_nav ul li').eq(destination.index).addClass('active')
+			
 		},
 
-		responsiveWidth: 768 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+		responsiveWidth: 768
 	});
 
 	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
