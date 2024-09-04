874bddce0bfa66112f50b382173977d5386542eb
se0413 <tldms7133@naver.com>
2024-09-04 13:18:28 +0900

    0904.1

--- cheonan_museum_of_art/js/main.js
+++ cheonan_museum_of_art/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-	$()
+
+	setTimeout(function(){
+		$('.visual > strong').addClass('mr')
+	}, 1000);
+	setTimeout(function(){
+		$('.visual h1 > strong:nth-child(1).mr').removeClass('mr')
+	}, 3000);
+
 })
